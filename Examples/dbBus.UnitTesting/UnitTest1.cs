cafa035a36b1f030cdcb575d4ec7235adcbb09f4
Sebastian Bucko <sebastianbucko77@gmail.com>
2019-01-21 12:22:16 +0100

    unit testing example added

---
+++ Examples/dbBus.UnitTesting/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using dbBus.Core;
+using dbBus.Extensions.NInject;
+
+using Ninject;
+using Xunit;
+
+namespace dbBus.UnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var kernel = new StandardKernel();
+
+            var bus = Bus.Configure()
+              .UseNinject(kernel)
+              .UseSqlite(":memory:")
+              .RegisterHandler<MyMessageHandler1>()
+              .RegisterHandler<MyMessageHandler2>()
+              .RegisterHandler<MyMessageHandler3>()
+              .Build();
+
+            bus.Start();
+        }
+    }
+}
